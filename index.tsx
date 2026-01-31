280b1a96aea900a57a1c7d882b3700fa9e93e13b
trioncee <trioncee@gmail.com>
2026-01-31 22:31:52 +0530

    Update index

--- index.tsx
+++ index.tsx
@@ -2,6 +2,7 @@
 import React, { useEffect } from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import './index.css';
 
 const GrainEffect = () => {
   useEffect(() => {
